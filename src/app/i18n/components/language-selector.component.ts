7faffc1679107663f9eddca3925ac2cd1babbfa5
Rihabb-A <rihab@sirenanalytics.com>
2026-05-02 00:58:37 +0300

    Enhancement

--- src/app/i18n/components/language-selector.component.ts
+++ src/app/i18n/components/language-selector.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, EventEmitter, HostListener, Input, Output, inject } from '@angular/core';
 import { LanguageOption } from '../language.model';
+import { TranslationService } from '../services/translation.service';
 
 @Component({
   selector: 'app-language-selector',
@@ -8,6 +9,7 @@ import { LanguageOption } from '../language.model';
 })
 export class LanguageSelectorComponent {
   private readonly elementRef = inject(ElementRef<HTMLElement>);
+  private readonly translationService = inject(TranslationService);
 
   @Input({ required: true }) languages: readonly LanguageOption[] = [];
   @Input({ required: true }) selectedLanguage!: LanguageOption;
@@ -22,6 +24,7 @@ export class LanguageSelectorComponent {
   }
 
   protected selectLanguage(language: LanguageOption): void {
+    this.translationService.setLanguage(language.code);
     this.selectedLanguageChange.emit(language);
     this.isOpen = false;
   }
--- src/app/i18n/index.ts
+++ src/app/i18n/index.ts
@@ -2,3 +2,4 @@ export * from './auth.translations';
 export * from './language.model';
 export * from './languages';
 export * from './components/language-selector.component';
+export * from './services/translation.service';
